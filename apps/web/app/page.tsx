8937eab3c01168efbcd9706557610bfb4273f04d
abelva20 <belvajanitra205@gmail.com>
2025-08-31 21:31:50 +0700

    Package Convex

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,17 @@
-import { Button } from "@workspace/ui/components/button"
-import { add } from "@workspace/math/add"
+"use client"
+
+import {useQuery} from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
 export default function Page() {
+  const users = useQuery(api.users.getMany);
   return (
     <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello apps/web</h1>
-        <Button size="sm">Button</Button>
-        <p>{add(2, 5)}</p>
-      </div>
+        <div className="flex flex-col text-sm justify-center items-center">
+          <p>
+            Apps/webs {""}
+            {users ? `Found ${users.length} users! ${JSON.stringify(users, null, 2)}` : "Loading..."}
+          </p>
+        </div>
     </div>
   )
 }
--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,12 +1,27 @@
-import { Button } from "@workspace/ui/components/button"
+"use client"
 
+import {useMutation, useQuery} from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
+import { Button } from "@workspace/ui/components/button";
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add)
+
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello apps/widget</h1>
-        <Button size="sm">Button</Button>
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <h1 className="text-2xl font-bold mb-4">
+        Apps/widget
+      </h1>
+      <Button
+        onClick={() => addUser()}
+      >
+        Add
+      </Button>
+        <div className="flex flex-col text-sm max-w-sm mt-5">
+          <p>
+            {users ? `Found ${users.length} users! ${JSON.stringify(users, null, 2)}` : "Loading..."}
+          </p>
+        </div>
     </div>
   )
 }
